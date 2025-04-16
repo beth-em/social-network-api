396137b85370f0509f9003c2f983b7722c32b049
beth-em <beth.murillo318@gmail.com>
2025-04-15 22:58:32 -0600

    fixed error in thoughtRoutes.js to allow users to create a reaction without 404 error

--- Develop/routes/api/thoughtRoutes.js
+++ Develop/routes/api/thoughtRoutes.js
@@ -15,9 +15,12 @@ router.route('/:thoughtId')
     .put(thoughtController.updateThought)      // PUT update a thought
     .delete(thoughtController.deleteThought);  // DELETE a thought
 
+// Add routes for /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(thoughtController.addReaction);         // POST add a reaction
+
 // Add routes for /api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId')
-    .post(thoughtController.addReaction)         // POST add a reaction
     .delete(thoughtController.removeReaction);  // DELETE a reaction
 
 export default router;
